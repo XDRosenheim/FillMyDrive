6d7f1bf98471d1a898c158f9e3ad1556cc075352
XDRosenheim <patrickrosenheim@gmail.com>
2016-01-27 11:06:19 +0100

    Version.
    I always forget this...

--- FillMyDrive/Properties/AssemblyInfo.cs
+++ FillMyDrive/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "FillMyDrive" )]
-[assembly: AssemblyDescription( "This program will fill your drive, slowly..." )]
+[assembly: AssemblyDescription( "This program will fill your drive. Slooowly..." )]
 [assembly: AssemblyCompany( "rosenheim.dk" )]
 [assembly: AssemblyProduct( "FillMyDrive" )]
 [assembly: AssemblyCopyright( "Copyright © rosenheim.dk 2016" )]
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.1.*" )]
-[assembly: AssemblyFileVersion( "1.0.1.0" )]
+[assembly: AssemblyVersion( "1.1.0.*" )]
+[assembly: AssemblyFileVersion( "1.1.0.0" )]
